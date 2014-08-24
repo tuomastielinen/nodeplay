d4df3abdf1bd177495565ca76bedc23cc6d63350
Tuomas Tielinen <tuomas.tielinen@gmail.com>
2014-08-24 23:05:38 +0300

    chapter 12

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 var bodyParser = require('body-parser');
@@ -21,13 +22,63 @@ var handlebars = require('express-handlebars').create({
     }
 });
 
-emailService.send('first.last@gmail.com', 'Hood River tours on sale today!',
-    'Get \'em while they\'re hot!');
+// emailService.send('first.last@gmail.com', 'Hood River tours on sale today!',
+//     'Get \'em while they\'re hot!');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+
+try {
+    // use domains for better error handling
+    app.use(function(req, res, next) {
+        // create a domain for this request
+        var domain = require('domain').create();
+        // handle errors on this domain
+        domain.on('error', function(err) {
+            console.error('DOMAIN ERROR CAUGHT\n', err.stack);
+            try {
+                // failsafe shutdown in 5 seconds
+                setTimeout(function() {
+                    console.error('Failsafe shutdown.');
+                    process.exit(1);
+                }, 5000);
+
+                // disconnect from the cluster
+                var worker = require('cluster').worker;
+                if (worker) worker.disconnect();
+
+                // stop taking new requests
+                server.close();
+
+                try {
+                    // attempt to use Express error route
+                    next(err);
+                } catch (error) {
+                    // if Express error route failed, try
+                    // plain Node response
+                    console.error('Express error mechanism failed.\n', error.stack);
+                    res.statusCode = 500;
+                    res.setHeader('content-type', 'text/plain');
+                    res.end('Server error.');
+                }
+            } catch (error) {
+                console.error('Unable to send 500 response.\n', error.stack);
+            }
+        });
+
+        // add the request and response objects to the domain
+        domain.add(req);
+        domain.add(res);
+
+        // execute the rest of the request chain in the domain
+        domain.run(next);
+    });
+} catch (err) {
+    console.log(err.stack);
+}
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -64,6 +115,29 @@ app.use('/upload', function(req, res, next) {
     })(req, res, next);
 });
 
+
+switch (app.get('env')) {
+    case 'development':
+        // compact, colorful dev logging
+        app.use(require('morgan')('dev'));
+        break;
+    case 'production':
+        // module 'express-logger' supports daily log rotation
+        app.use(require('express-logger')({
+            path: __dirname + '/log/requests.log'
+        }));
+        break;
+}
+
+app.get('/fail', function(req, res) {
+    throw new Error('Nope!');
+});
+
+app.get('/epic-fail', function(req, res) {
+    process.nextTick(function() {
+        throw new Error('Kaboom!');
+    });
+});
 
 app.get('/contest/vacation-photo', function(req, res) {
     var now = new Date();
@@ -272,14 +346,23 @@ app.use(function(req, res, next) {
     res.render('404');
 });
 
-// 500 error handler (middleware)
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.status(500);
-    res.render('500');
+    app.status(500).render('500');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.');
-});
+function startServer() {
+    http.createServer(app).listen(app.get('port'), function() {
+        console.log('Express started in ' + app.get('env') +
+            ' mode on http://localhost:' + app.get('port') +
+            '; press Ctrl-C to terminate.');
+    });
+}
+if (require.main === module) {
+    // application run directly; start app server
+    startServer();
+} else {
+    // application imported as a module via "require": export function
+    // to create server
+    module.exports = startServer;
+}
