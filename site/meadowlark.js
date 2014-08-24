068ddf1a611f2274f145d930e4ca95e980740cea
Tuomas Tielinen <tuomas.tielinen@gmail.com>
2014-08-24 18:30:57 +0300

    chapter 11

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
 var credentials = require('./credentials.js');
+var nodemailer = require('nodemailer');
+var emailService = require('./lib/email.js')(credentials);
 
 var app = express();
 
@@ -18,6 +20,9 @@ var handlebars = require('express-handlebars').create({
         }
     }
 });
+
+emailService.send('first.last@gmail.com', 'Hood River tours on sale today!',
+    'Get \'em while they\'re hot!');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -224,6 +229,40 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res) {
         console.log('received files:');
         console.log(files);
         res.redirect(303, '/thank-you');
+    });
+});
+
+app.post('/cart/checkout', function(req, res) {
+    var cart = req.session.cart;
+    if (!cart) next(new Error('Cart does not exist.'));
+    var name = req.body.name || '',
+        email = req.body.email || '';
+    // input validation
+    if (!email.match(VALID_EMAIL_REGEX))
+        return res.next(new Error('Invalid email address.'));
+    // assign a random cart ID; normally we would use a database ID here
+    cart.number = Math.random().toString().replace(/^0\.0*/, '');
+    cart.billing = {
+        name: name,
+        email: email,
+    };
+    res.render('email/cart-thank-you', {
+        layout: null,
+        cart: cart
+    }, function(err, html) {
+        if (err) console.log('error in email template');
+        mailTransport.sendMail({
+            from: '"Meadowlark Travel": info@meadowlarktravel.com',
+            to: cart.billing.email,
+            subject: 'Thank You for Book your Trip with Meadowlark',
+            html: html,
+            generateTextFromHtml: true
+        }, function(err) {
+            if (err) console.error('Unable to send confirmation: ' + err.stack);
+        });
+    });
+    res.render('cart-thank-you', {
+        cart: cart
     });
 });
 
